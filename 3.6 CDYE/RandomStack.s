c3f2a200f0276ce04d4f5419f542d3be8539af48
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-09 17:07:09 -0800

    Update RandomStack.s

--- 3.6 CDYE/RandomStack.s
+++ 3.6 CDYE/RandomStack.s
@@ -1,23 +1,13 @@
-//@@ Instituto Tecnologico de Tijuana
-//@@ Depto de Sistemas y Computación
-//@@ Ing.En Sistemas Computacionales
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 13/12/2021*/
 
-
-
-//@ Objetivo del programa :
-
-Ejemplo muy sencillo  del uso del stack de la cpu.
-*/
-
-// Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
-// Basically Push() and Pop() as in stacks. Hmm, interesting stuff for me as a beginner.
-
-//.section // __TEXT, __text
 .global main
 
 main:
-     mov   r0, #2  /* set up r0 */
-     push  {r0}    /* save r0 onto the stack */
-     mov   r0, #3  /* overwrite r0 */
-     pop   {r0}    /* restore r0 to it's initial state */
-     bx    lr      /* finish the program */
+     mov   r0, #2  
+     push  {r0}    
+     mov   r0, #3  
+     pop   {r0}    
+     bx    lr      
