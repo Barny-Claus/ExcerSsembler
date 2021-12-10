f804631611905bbe7612809f53c6642eb54cfa9b
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-09 16:15:31 -0800

    Update Hannoi.s

--- 3.6 CDYE/Hannoi.s
+++ 3.6 CDYE/Hannoi.s
@@ -1,70 +1,45 @@
-@   hanoi2.s
-@   D. Thiebaut
-@   Recursive towers of Hanoi
-@   Prompts the user for the number of disks to 
-@   move.
-//as -o hanoi.o hanoi.s
-//gcc -o hanoi hanoi.o
-@ ---------------------------------------
-@	Data Section
-@ ---------------------------------------
-	
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* 13/12/2021
+*/
 	.data
 	.balign 4
-prompt:	.asciz	"How many disk do you want to move? "
+prompt:	.asciz	"Cuantos discos quieres mover? "
 format:	.asciz	"%d"	
-string: .asciz  "move disk from %c to %c\n\0"
-peg1:	.int	'A'		@ name of Peg 1, must be a char
+string: .asciz  "mover el disco de %c a %c\n\0"
+peg1:	.int	'A'		
 peg2:	.int	'B'
 peg3:	.int	'C'
-n:	.int	4		@ number of disks to move
-
-	
-@ ---------------------------------------
-@	Code Section
-@ ---------------------------------------
+n:	.int	4		
 	
 	.text
 	.global main
 	.extern printf
 	.extern	scanf
 
-@ ---------------------------------------
-@ getNumberOfDisks():
-@ prompts the user and puts the number
-@ in n.
 getNumberOfDisks:
-	push 	{ip, lr}	@ push return address + dummy register
-				@ for alignment
+	push 	{ip, lr}	
 
-	ldr	r0, =prompt	@ print the prompt
+	ldr	r0, =prompt	
 	bl	printf
 
-	ldr     r0, =format	@ call scanf, and pass address of format
-	ldr	r1, =n		@ string and address of n in r0, and r1,
-	bl	scanf		@ respectively.
-
+	ldr     r0, =format	//llamamos al scanf
+	ldr	r1, =n		
+	bl	scanf		
         pop 	{ip, pc}
 	
-@ ---------------------------------------
-@ moveDisks( n, Source, Dest, Extra )
-@	    r4   r5     r6    r7
-@    if n==1:
-@	printf( "move disk from %c to %c\n", Source, Dest )
-@    else:
-@	moveDisks( n-1, Source, Extra, Desk )
-@	printf( "move disk from %c to %c\n", Source, Dest )
-@	moveDisks( n-1, Extra, Dest, Source )
-@	
-moveDisks:	
+	
+moveDisks:	// n, Source, Destino, Extra
 	push	{r4-r8, lr}
 
 	@ if n==1:
-	cmp	r4, #1
-	bgt	moveN_1Disks
+	cmp	r4, #1 //comparamos si r4 (n) es igual a 1
+			//sino repetimos movedisck pero con n-1 e imprimimos
+	bgt	moveN_1Disks 
 
 move1Disk:	
-	@ print( "move disk from %c to %c\n", Source, Dest )
+	// llamamos a print( mover el disco de %c a %c\n, Source, Dest )
 	ldr	r0, =string
 	mov	r1, r5
 	mov	r2, r6
@@ -72,24 +47,24 @@ move1Disk:
 	b	endMoveDisk
 
 moveN_1Disks:	
-	@ moveDisks( n-1, Source, Extra, Dest )
-	mov	r8, r7		@ swap Dest & Extra
+	// moveDisks( n-1, Source, Extra, Destino )
+	mov	r8, r7		
 	mov	r7, r6
 	mov	r6, r8
 	sub	r4, #1
 	bl	moveDisks
-	mov	r8, r7		@ unswap Dest & Extra
+	mov	r8, r7		
 	mov	r7, r6
 	mov	r6, r8
 
-	@ print( "move disk from %c to %c\n", Source, Dest )
+	// llamamos a print( mover el disco de %c a %c\n, Source, Dest )
 	ldr	r0, =string
 	mov	r1, r5
 	mov	r2, r6
 	bl	printf
 
-	@ moveDisks( n-1, Extra, Dest, Source )	
-	mov	r8, r5		@ swap Source and Extra, i.e. r5 and r7.
+	// mandamos moveDisks( n-1, Extra, Dest, Source )	
+	mov	r8, r5		
 	mov	r5, r7
 	mov	r7, r8
 	bl	moveDisks
@@ -98,25 +73,19 @@ endMoveDisk:
 	pop	{r4-r8, pc}
 
 	
-@ ---------------------------------------
-@ main: call moveDisks( n, peg1, peg2, peg3 )
 
-main:   push 	{ip, lr}	@ push return address + dummy register
-				@ for alignment
-
-	bl	getNumberOfDisks
-				@ get n
-	
-	ldr	r4, =n		@ pass n
+main:   push 	{ip, lr}	
+	bl	getNumberOfDisks //numero de discos
+	ldr	r4, =n		
 	ldr	r4, [r4]
-	ldr	r5, =peg1	@ pass peg1
+	ldr	r5, =peg1	
 	ldr	r5, [r5]
-	ldr	r6, =peg2	@ pass peg2
+	ldr	r6, =peg2	
 	ldr	r6, [r6]
-	ldr	r7, =peg3	@ pass peg3
+	ldr	r7, =peg3	
 	ldr	r7, [r7]
-	bl	moveDisks	@ call moveDisks( n, peg1, peg2, peg3 )
+	bl	moveDisks	
 
 	
 @ return to OS	
-        pop 	{ip, pc}	@ pop return address into pc
+        pop 	{ip, pc}	
