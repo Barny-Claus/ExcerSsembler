17ebe23a2fa60dd0d19e1d05b5e393ac1282878e
Gamaliel Rodriguez <89487724+Barny-Claus@users.noreply.github.com>
2021-12-09 17:10:15 -0800

    Update Teclado.s

--- 3.6 CDYE/Teclado.s
+++ 3.6 CDYE/Teclado.s
@@ -1,46 +1,49 @@
-@ greet.s - a little asm greeter.
-@ programa corto que obtiene la entrada del teclado y luego lo imprime de nuevo en la pantalla.
+/*Instito tecnológico de Tijuana
+* Lenguaje de interfaz
+* Autor: Rodriguez Moreno Omar Gamaliel
+* programa corto que obtiene la entrada del teclado y luego lo imprime de nuevo en la pantalla.
+* 13/12/2021 */
 
 
 .section	.bss
-.comm buffer, 48	     @ reserve 48 byte buffer
+.comm buffer, 48	   
 
 .section	.data
 msg:
-	.ascii	"** Greeter **\nPlease enter your name: "
+	.ascii	"** Greeter **\nIntroduzca su nombre: "
 msgLen = . - msg
 msg2:
-	.ascii	"Hello "
+	.ascii	"Hola "
 msg2Len = . - msg2
 
 .section	.text
 .globl	_start
 _start:
 
-mov r0, $1		    @ print program's opening message	
+mov r0, $1		   
 ldr r1, =msg
 ldr r2, =msgLen
 mov r7, $4
 svc $0
 
-mov r7, $3		    @ read syscall
+mov r7, $3		    //leemos
 mov r0, $1		
 ldr r1, =buffer
 mov r2, $0x30
 svc $0
 
-mov r0, $1		    @ print msg2
+mov r0, $1		    //imprimimos msg2
 ldr r1, =msg2
 ldr r2, =msg2Len
 mov r7, $4
 svc $0
 
-mov r0, $1		    @ now print the user input
+mov r0, $1		    //imrpimimos el nombre
 ldr r1, =buffer
 mov r2, $0x30
 mov r7, $4
 svc $0
 
-mov r7, $1	            @ exit syscall
-svc $0		            @ wake kernel
+mov r7, $1	           
+svc $0		           
 .end
